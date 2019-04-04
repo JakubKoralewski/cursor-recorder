8bf14d66cd04c405a5bf09232c2b9cbe852f9e12
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-04-04 19:44:08 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='cursor-recorder',
-    version='0.1',
+    version='0.2',
     description='Records cursor data',
     license='MIT',
     long_description=long_description,
