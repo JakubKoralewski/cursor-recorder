1e4fecf2b4c95823eab3c1f9dc40f49e5df2a70f
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-04-04 19:43:06 +0200

    not needed

--- build_script.sh
+++
@@ -1,4 +0,0 @@
-rm -r release
-mkdir release
-sed -n '/include/!p' afterfx-script/cursor-recorder.jsx > release/cursor-recorder.jsx
-sed -i '/* *\// r afterfx-script/json2.jsxinc' release/cursor-recorder.jsx
