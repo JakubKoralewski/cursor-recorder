b9a84a9dfc60adc09743c9527c0ed0a0cfb3fe59
Jakub Koralewski <contact@jcubed.me>
2019-12-09 13:43:55 +0100

    Fixed auto installing packages

--- scripts/cursor_recorder_for_obs.py
+++ scripts/cursor_recorder_for_obs.py
@@ -2,50 +2,113 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 # Copyright 2019 (c) Jakub Koralewski
-
 import obspython as obs
 import datetime
 import os
-from subprocess import call
+from subprocess import Popen, PIPE
+from typing import List
 import time
 import threading
 
 import logging
 import sys
 
-import pyautogui
-import keyboard
-
 file_handler = logging.FileHandler(filename='obs_cursor_recorder.log')
 stdout_handler = logging.StreamHandler(sys.stdout)
 handlers = [file_handler, stdout_handler]
 logging.basicConfig(
-    level=logging.DEBUG, 
-    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
-    handlers=handlers
+	level=logging.DEBUG,
+	format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+	handlers=handlers
 )
 logger = logging.getLogger('obs_cursor_recorder_logger')
+
+p = sys.platform
+py_executable = "python.exe"
+if p == "linux" or p == "linux2":
+	py_executable = "python3.6"
+elif p == "win32" or p == "win64":
+	py_executable = "python.exe"
+else:
+	logging.error(f"Unsupported platform: {p}\nWindows or Linux.")
+del p
 
 import_succeeded = False
 cached_settings = {}
 properties = {}
 is_being_recorded = False
 
+
 def now():
 	return datetime.datetime.now().time()
 
-logger.info(f"{now()}: refresh")
 
-py_interpreter = os.path.join(os.__file__.split("lib")[0], "python.exe")
+logger.info(f"{now()}: refresh")
+py_dir = os.__file__.split("lib")[0]
+py_interpreter = os.path.join(py_dir, py_executable)
 logger.info(f'py_interpreter: {py_interpreter}')
 
 output = obs.obs_frontend_get_recording_output()
 
 SHOULD_EXIT = False
 
-
 name = 'obs_cursor_recorder.txt'
 path = 'C:/Users/Admin/Documents'
+
+
+def install_pip_then_multiple(packages):
+	logger.info('Import pip then multiple modules.')
+	assert (os.path.isdir(py_dir))
+
+	"""
+	:param name - name of action you are doing, for logging 
+	"""
+
+	def install(c: List, name: str = ""):
+		logger.info(name)
+		p = Popen(
+			' '.join(c),
+			stdout=PIPE,
+			stderr=PIPE,
+			stdin=PIPE,
+			executable=py_interpreter,
+			cwd=py_dir[:-1]
+		)
+		print(f"Stdout writable: {p.stdout.writable()}")
+		print(p.args)
+		out, err = p.communicate()
+		if len(err) != 0:
+			logging.error(f"{name} Error:\n {err}")
+		logging.info(f"{name} Output:\n {out}")
+
+	cmd_line_update = [
+		py_executable, '-m', 'pip', 'install', '--upgrade', 'pip', '--user'
+	]
+
+	cmd_line_install = [
+		py_executable, '-m', 'pip', 'install'
+	] + packages
+
+	logger.debug(f'Python interpreter location: {py_interpreter}')
+	logger.debug(f'Python interpreter location directory: {py_dir}')
+	install(cmd_line_update, "Upgrading your pip version: ")
+	install(cmd_line_install, "Installing packages: ")
+
+
+def install_modules_button_click():
+	install_pip_then_multiple(['pyautogui', 'keyboard'])
+
+
+try:
+	import pyautogui
+	import keyboard
+except ModuleNotFoundError:
+	logger.error("PyAutoGui and or keyboard are not installed.\n\
+I'm installing them for you if you don't mind.\n")
+	install_pip_then_multiple(['pyautogui', 'keyboard'])
+	import pyautogui
+	import keyboard
+
 
 def save_to_file(seconds, x, y):
 	full_path = os.path.join(path, name) if path and name else "cursor-recorder.txt"
@@ -54,6 +117,7 @@ def save_to_file(seconds, x, y):
 	with open(full_path, "a+") as file:
 		file.write(f'{seconds} {x} {y}\n')
 
+
 x = -1
 y = -1
 prev_x = -1
@@ -61,10 +125,11 @@ prev_y = -1
 seconds = 0
 skipping = False
 
+
 def script_tick(time_passed):
 	if not is_being_recorded or not cached_settings["use_default_fps"]:
 		return
-	
+
 	global x
 	global y
 	global prev_x
@@ -95,20 +160,22 @@ def script_tick(time_passed):
 
 	save_to_file(seconds, x, y)
 
+
 def should_exit():
 	if SHOULD_EXIT:
 		logger.info('should exit!')
 		return True
 	return False
 
+
 def cursor_recorder():
-	refresh_rate = 1 / cached_settings["custom_fps"] 
+	refresh_rate = 1 / cached_settings["custom_fps"]
 	IS37 = True
 	try:
 		time.time_ns()
 	except AttributeError:
 		IS37 = False
-	
+
 	x = -1
 	y = -1
 	prev_x = -1
@@ -162,7 +229,8 @@ Docs:
 	- https://obsproject.com/docs/frontends.html#signals 
 """
 
-def recording_start_handler(x):
+
+def recording_start_handler(_):
 	global is_being_recorded
 	global path
 	global name
@@ -170,6 +238,7 @@ def recording_start_handler(x):
 
 	logger.info(f'recording started ({now()})')
 	output_settings = obs.obs_output_get_settings(output)
+	logger.debug(output_settings)
 
 	""" Example path: "C:/Users/Admin/Documents/2019-04-04 16-02-28.flv"
 	After `os.path.split(path)`: ('C:/Users/Admin/Documents', '2019-04-04 16-02-28.flv')"""
@@ -186,23 +255,23 @@ def recording_start_handler(x):
 		logger.info('Starting recording using custom FPS settings.')
 		threading.Thread(target=cursor_recorder).start()
 
-def install_modules_button_click(x, y):
-	install_pip_then_multiple(['pyautogui', 'keyboard'])
 
-def recording_stopped_handler(x):
+def recording_stopped_handler(_):
 	global is_being_recorded
 	global SHOULD_EXIT
 	SHOULD_EXIT = True
 	is_being_recorded = False
 	logger.info(f'recording stopped ({now()})')
 
-def install_pip_then_multiple(packages):
-	logger.info('Import pip then multiple modules.')
-	call([py_interpreter, '-m', 'pip', 'install', '--upgrade', 'pip', '--user', '&&', py_interpreter, '-m', 'pip', 'install'] + packages)
 
 def script_description():
 	logger.debug('script_description')
-	return '<h1>OBS Cursor Recorder</h1><b>Lets you save your cursor movement to a file.</b><br/><br/>©2019 Jakub Koralewski. <a href="https://github.com/JakubKoralewski/cursor-recorder-for-afterfx" target="_blank">Open-source on GitHub.</a> <hr />  '
+	return '<h1>OBS Cursor Recorder</h1><b>Lets you save your cursor movement to a file.</b>' \
+		'<br/><br/>©2019 Jakub Koralewski. ' \
+		'<a href="' \
+		'https://github.com/JakubKoralewski/cursor-recorder-for-afterfx"' \
+		'target="_blank">Open-source on GitHub.</a> <hr />'
+
 
 def script_properties():
 	logger.debug('script_properties')
@@ -210,7 +279,6 @@ def script_properties():
 	enabled = obs.obs_properties_add_bool(props, "enabled", "Enabled")
 	obs.obs_property_set_long_description(enabled, "Whether to save the file when recording or not.")
 
-	
 	long_default_fps_description = "Use this option to achieve higher accuracy than every video frame."
 
 	default_fps = obs.obs_properties_add_bool(props, "use_default_fps", "Use video's FPS to capture cursor")
@@ -219,17 +287,25 @@ def script_properties():
 	custom_fps = obs.obs_properties_add_int_slider(props, "custom_fps", "Custom FPS", 1, 200, 1)
 	obs.obs_property_set_long_description(custom_fps, long_default_fps_description)
 
-	install_modules = obs.obs_properties_add_button(props, "install_modules", "Install Python modules", install_modules_button_click)
-	
-	obs.obs_property_set_long_description(install_modules, "Installs pip, pyautogui and keyboard Python modules in your specified Python interpreter.")
+	install_modules = obs.obs_properties_add_button(
+		props,
+		"install_modules",
+		"Install Python modules",
+		install_modules_button_click
+	)
 
-	
+	obs.obs_property_set_long_description(
+		install_modules,
+		"Installs pip, pyautogui and keyboard Python modules in your specified Python interpreter."
+	)
+
 	return props
 
 
 def script_save(settings):
 	logger.debug('script_save')
 	script_update(settings)
+
 
 def script_update(settings):
 	logger.debug('script_update')
@@ -255,11 +331,9 @@ def script_update(settings):
 
 	logger.debug(f'cached_settings: {cached_settings}')
 
+
 def script_defaults(settings):
 	logger.debug('script_defaults')
 	obs.obs_data_set_default_bool(settings, "enabled", True)
 	obs.obs_data_set_default_bool(settings, "use_default_fps", True)
 	obs.obs_data_set_default_int(settings, "custom_fps", 30)
-
-
-
